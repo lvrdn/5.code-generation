18969e9c070d6d140c03b4e01085cfec8a98d7f6
Денис Лавринов <da.lavrinov@yandex.ru>
2024-09-16 10:39:40 +0300

    fix some comments

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-// это программа для которой ваш кодогенератор будет писать код
-// запускать через go test -v, как обычно
-
-// этот код закомментирован чтобы он не светился в тестовом покрытии
-
 import (
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	// будет вызван метод ServeHTTP у структуры MyApi
+
 	http.Handle("/user/", NewOtherApi())
 
 	fmt.Println("starting server at :8080")
